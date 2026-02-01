00382264ea497eb312e003888603701b9c77f6d0
yusufkocak01 <ysfkck@gmail.com>
2026-02-02 01:16:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,29 @@ from flask import Flask, request, send_file
 import subprocess, uuid, os
 
 app = Flask(__name__)
+
 LOGO_PATH = "logo.png"
+
+@app.route("/")
+def home():
+    return "Watermark API çalışıyor"
 
 @app.route("/watermark", methods=["POST"])
 def watermark():
+    if 'video' not in request.files:
+        return "Video yok", 400
+
     video = request.files['video']
+
     in_file = f"/tmp/{uuid.uuid4()}.mp4"
     out_file = f"/tmp/out_{uuid.uuid4()}.mp4"
 
     video.save(in_file)
 
     cmd = [
-        "ffmpeg", "-i", in_file, "-i", LOGO_PATH,
+        "ffmpeg",
+        "-i", in_file,
+        "-i", LOGO_PATH,
         "-filter_complex",
         "[1]format=rgba,colorchannelmixer=aa=0.6[logo];[0][logo]overlay=(main_w-overlay_w)/2:main_h-overlay_h-60",
         "-codec:a", "copy",
